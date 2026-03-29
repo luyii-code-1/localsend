7796b1f049687dbded528fbea22b12eed36ed44e
luyii-code-1 <112700386+luyii-code-1@users.noreply.github.com>
2026-03-29 13:58:26 +0800

    ui: add bluetooth symbol prefix to discovered device names

---
+++ app/lib/provider/bluetooth_discovery_provider.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:localsend_app/util/native/channel/android_channel.dart';
+import 'package:localsend_app/util/native/platform_check.dart';
+import 'package:refena_flutter/refena_flutter.dart';
+
+final bluetoothDiscoveryProvider = StateProvider<List<BluetoothDeviceInfo>>((ref) {
+  return [];
+}, debugLabel: 'bluetoothDiscoveryProvider');
+
+final bluetoothSignalInfoProvider = StateProvider<Map<String, String>>((ref) {
+  return {};
+}, debugLabel: 'bluetoothSignalInfoProvider');
+
+class StartBluetoothDiscoveryAction extends AsyncGlobalAction {
+  @override
+  Future<void> reduce() async {
+    if (!checkPlatform([TargetPlatform.android])) {
+      return;
+    }
+
+    final devices = await scanBluetoothDevicesAndroid();
+    ref.notifier(bluetoothDiscoveryProvider).setState((_) => devices);
+
+    final signalInfo = await getBluetoothSignalInfoAndroid();
+    ref.notifier(bluetoothSignalInfoProvider).setState((_) => signalInfo);
+  }
+}
